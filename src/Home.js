937e27450149c93ae22de33fa019f870ce0c8c1f
bjohnson <bejohnson@marketo.com>
2017-12-14 15:33:25 -0800

    more stuff

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 class Home extends Component {
   render() {
     return (
-      <div className="nav-bubble">
-        <div className="nav-bubble-text">
-          <div>MEET MR. PUFFIN PUFFOUT</div>
-          <div>OUR FAVORITE ANIMAL EVER</div>
-          <div>HOWEVER, HE IS SAD AS HIS BROTHER MR PUFFYY IS MISSING, HELP HIM FIND HIS BRO!!!</div>
-        </div>
+      <div className="home-div">
+        <ul>
+          <li><Link to="about">About</Link></li>
+          <li><Link to="gallery">Gallery</Link></li>
+          <li><Link to="diet">Diet</Link></li>
+          <li><Link to="anatomy">Anatomy</Link></li>
+          <li><Link to="science">Science</Link></li>
+        </ul>
       </div>
     );
   }
