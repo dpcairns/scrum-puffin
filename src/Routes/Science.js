0b9637c1af716745f796f46777e68e8386186389
bjohnson <bejohnson@marketo.com>
2017-12-14 15:48:00 -0800

    more stuff

--- src/Routes/Science.js
+++ src/Routes/Science.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 class Science extends Component {
   render() {
     return (
-      <div>
-        Science about puffins!
+      <div className="science-div">
+        <ul>
+          <li><Link to="diet">Diet</Link></li>
+          <li><Link to="gallery">Gallery</Link></li>
+          <li><Link to="anatomy">Physical Attributes</Link></li>
+          <li><Link to="science">Habitat</Link></li>
+        </ul>
       </div>
     );
   }
