f7b8ba82ac8bbce1a88a3ac693cfd2492edbfc1f
dcairns <dcairns@marketo.com>
2017-12-15 11:22:46 -0800

    hmm

---
+++ src/Routes/Anatomy.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Anatomy extends Component {
+  render() {
+    return (
+      <div>
+        Puffin anatomy!
+      </div>
+    );
+  }
+}
+
+export default Anatomy;
---
+++ src/Routes/Behavior.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Behavior extends Component {
+  render() {
+    return (
+      <div className="behavior-page">
+      </div>
+    );
+  }
+}
+
+export default Behavior;
