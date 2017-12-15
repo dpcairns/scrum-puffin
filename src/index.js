3b18e674ac3c003fd9482ac65c5eeeec7f519f92
dcairns <dcairns@marketo.com>
2017-12-15 11:21:36 -0800

    reset

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Routing from './Routing.js';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Routing />, document.getElementById('root'));
-registerServiceWorker();
